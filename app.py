8c4799ccc04b24633b7c5f2bd3e01c72505ae3c9
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2021-02-07 12:34:17 +0200

    basic usability for inventory and diary

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask,request
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from sqlalchemy import sql
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    #Tarkistetaan tunnukset
+    session["usernmae"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 
 @app.route("/inventaario", methods=["GET", "POST"])
