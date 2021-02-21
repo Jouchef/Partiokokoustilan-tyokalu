e31ae73b940ff6bd7920a3ee2e945d1cc2c5a443
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2021-02-21 23:03:26 +0200

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -38,3 +38,19 @@ VALUES ('sadmin@sadmin.sadmin', 'pbkdf2:sha256:150000$RycREZON$f1a586c9cf0c62d99
 
 INSERT INTO users (username, password, role)
 VALUES ('admin@admin.admin', 'pbkdf2:sha256:150000$0ZoDK7GW$73dd38394ae61cf60edb81a5651b6a5d7a0ba2b78d41026a468815c60f06f3e0', 'admin');
+
+
+
+
+
+CREATE TABLE laskuri 
+(id SERIAL PRIMARY KEY, realpvm DATE DEFAULT CURRENT_DATE, ryhma TEXT, pvm DATE, nuoria INTEGER, aikuisia INTEGER, ulkopaikkakuntalainen INTEGER);
+
+INSERT INTO laskuri (ryhma, pvm, nuoria, aikuisia, ulkopaikkakuntalainen)
+VALUES ("Ryhmä A", "2021-02-21", 10, 2, 1);
+
+INSERT INTO laskuri (ryhma, pvm, nuoria, aikuisia, ulkopaikkakuntalainen)
+VALUES ("Tulitähdet", "2021-02-20", 13, 3, 0);
+
+INSERT INTO laskuri (ryhma, pvm, nuoria, aikuisia, ulkopaikkakuntalainen)
+VALUES ("Puumat", "2021-02-19", 9, 2, 0);
